c000be3ec897db53516557f543271d4473469fd6
Wiz <phamnahtbao1991@gmail.com>
2022-08-25 20:17:25 +0700

    Update project config

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+function Footer(): JSX.Element {
+  return (
+    <footer className='text-center bg-state-100 bg-black'>
+      <div className='max-w-screen-xl px-4 py-3 mx-auto sm:px-6 lg:px-8'>
+        <div className='max-w-3xl mx-auto space-y-6 text-white'>
+          <p className='text-xs font-medium'>2022 Wiz Pham</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
