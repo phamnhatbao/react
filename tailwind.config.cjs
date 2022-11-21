c401c26e915cd2230dad7065c461574f4bc36723
Bảo Nhật Phạm <bao.pham@adnovum.vn>
2022-11-21 23:18:15 +0700

    Convert from Webpack to Vite

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,tsx}',
+    './index.html',
+    './src/**/*.{ts,tsx}'
   ],
   theme: {
     extend: {},
